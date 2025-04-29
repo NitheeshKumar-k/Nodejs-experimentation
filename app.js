32e7eb8a6694550f3febe074c79a3e2b03ae4df9
Nitheesh Kumar Kambala <nitheeshkumar13350@gmail.com>
2025-04-29 10:08:56 -0400

    Base project setup

---
+++ app.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const rootDir = require('./util/path');
+
+const app = express();
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(rootDir, 'public')));
+
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
+})
+
+app.listen(3000);
